4fdd99291467dc28102b24e3538afdbaadb9bf2b
SnitzelKlaus <SnitzelKlaus@gmail.com>
2025-02-26 00:11:37 +0100

    Added gitignore

--- src/BoligPletten.Core/BoligPletten.Infrastructure/Persistence/DbConnectionOptions.cs
+++ src/BoligPletten.Core/BoligPletten.Infrastructure/Persistence/DbConnectionOptions.cs
@@ -6,7 +6,7 @@
     public class DbConnectionOptions
     {
         public string ConnectionString { get; set; } = string.Empty;
-        public int MaxPoolSize { get; set; } = 1000; // Default to 1000
+        public int MaxPoolSize { get; set; } = 5000; // Default to 5000
         public int MinPoolSize { get; set; } = 10; // Default to 10
         public TimeSpan CommandTimeout { get; set; } = TimeSpan.FromSeconds(30);
     }
