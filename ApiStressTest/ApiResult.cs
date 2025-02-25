f8185d5b6b5c706a608a39b40fb2e57f112648cb
SnitzelKlaus <SnitzelKlaus@gmail.com>
2025-02-25 16:54:06 +0100

    Clean up

--- ApiStressTest/ApiResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiStressTest
-{
-    public class ApiResult
-    {
-        public HttpResponseMessage Response { get; set; } = new HttpResponseMessage();
-        public int CreatedId { get; set; } = 0;
-    }
-}
--- src/BoligPletten.Core/BoligPletten.Application/obj/Debug/net9.0/BoligPletten.Application.AssemblyInfo.cs
+++ src/BoligPletten.Core/BoligPletten.Application/obj/Debug/net9.0/BoligPletten.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoligPletten.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db7acd2fd2125fc8872a0edd9cd63052dead2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae234b8c07f17752325310bd7446985a1d33328")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoligPletten.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoligPletten.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
